115a914c16fd40d8cc5257605f5bd67749f8be68
albertojcalle <48741642+albertojcalle@users.noreply.github.com>
2022-02-19 23:19:37 +0100

    reestructure and main, condensate ok

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,38 @@
 module Main where
-
 import Lib
+import SAT.Mios ( solveSAT, CNFDescription(CNFDescription) )
+import SAT.Mios.Util.DIMACS ( fromFile )
+import System.IO (FilePath)
+import System.FilePath.Posix (makeRelative)
+
+ruta :: FilePath
+ruta = "src/Examples/original/cnf/uf20-01.cnf" 
 
 main :: IO ()
-main = print("Hello")
+main = do fileSolve ruta
+
+
+fileSolve :: FilePath -> IO ()
+fileSolve file =  do 
+    input <- fromFile ruta
+    case input of
+        Nothing -> error "Bad cnf file conversion."
+        Just tuple -> do
+            let 
+                ((nVar,nClauses), clauses) = tuple
+                description = CNFDescription nVar nClauses ruta
+            case solve clauses of
+                Left s -> putStrLn ( "Solution: " ++ show s)
+                Right c -> putStrLn ( "Contradiction: " ++ show c)
+
+
+miosSolve :: FilePath -> IO [Int]
+miosSolve file = do
+    input <- fromFile file
+    case input of
+        Nothing -> error "Bad cnf file conversion."
+        Just tuple -> do
+            let 
+                ((nVar,nClauses), clauses) = tuple
+                description = CNFDescription nVar nClauses file
+            solveSAT description clauses
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,96 +1,102 @@
 module Lib
     (
+    SatInfo,
+    Sat2,
+    solution,
     solve,
     condensate,
-    cnfToGraph,
-    opposite,
-    setValues
+    setValues,
+    cnfToGraph
     ) where
 
-import Data.Graph ( scc, buildG, path, Graph, Edge, Forest, Vertex)
+import Data.Graph ( buildG, path, scc, Edge, Graph ) 
 import Data.Tree ( flatten, drawForest )
-import Data.List (sort)
-
---TODO: Make a function solveSatInfo that finds all 4 values. 
+import Data.List ( sort )
 
 {-|
 All the information about a 2sat formula.
  -}
 data SatInfo = SatInfo {
-     solution :: Solution 
+     solution :: Solution
+    ,graph :: Graph 
     ,equivalences :: [Equivalence]
-    ,maxValue :: Int
-    ,solvable :: Bool
-    }
+    ,maxLiteral :: Int
+    ,solvable :: Maybe Bool
+    } deriving(Show)
+{-|
+Default record values for empty 2sat formula.
+-}
+satInfo :: SatInfo
+satInfo = SatInfo{
+     solution = []
+    ,graph = buildG (0,0) []
+    ,equivalences = [] 
+    ,maxLiteral = 0
+    ,solvable = Nothing 
+}
 type Sat2 = [[Int]]
 {-|
 A particular solution asigns a value T or F to every variable.  
  -}
 type Solution = [(Int, Bool)]
 {-|
-Variables that are equivalent to other variables within the same list. On a graph they are
+Variables that are equivalent to other variables on the original 2sat. On a graph they are
 strongly connected components.
-
-TODO: It may be used to give partial solutions.
 -}
-type Equivalence = (Int, [Int])
+type Equivalence = (Int, Scc)
 {-
 The strongly connected component that causes a contradiction. To be used when proof is needed that a formula is unsolvable.
 -}
 type Contradiction = [Int]
-
+{-|
+Strongly connected component.
+-}
+type Scc = [Int]
 {-|
 Supposedly scc uses Tarjan's algorithm, so the output is in reverse topological order. topSort should be avoided in this case as it is simpler just to reverse the order.
 
 TODO: Check topological order of scc output
-TODO: If there is no solution it should return the contradictions that make it unsolvable.
 -}
 solve :: Sat2 -> Either Solution Contradiction
-solve x =
+solve sat2 =
     let
-        satGraph = cnfToGraph x
+        (bounds, satGraph) = cnfToGraph sat2
         sccS = map (sort . flatten) $ scc satGraph 
-        condensation = condensate satGraph sccS
+        info = satInfo{
+             graph = satGraph
+            ,equivalences = zip ([1..] :: [Int]) sccS
+            ,maxLiteral = snd bounds}
         --solution = setValues condensation
-    in case condensation of
-        Right x -> Right x 
-        Left x ->  Left [] 
+    in case condensate info of
+        Right x -> Right x
+        Left x ->  Left [(1, True)]
 --TODO: Complete cases.
 
 {-|
 Either returns the first contradiction that makes the problem unsolvable or a list of equivalences in the formula. An equivalence is a sorted list of literals which could have the same value.
 -}
---condensate :: Sat2 -> [Equivalence] -> Either [Equivalence] Contradiction
---condensate :: Graph -> [Equivalence] -> Either [Equivalence] Contradiction
-condensate satGraph sccS =
+condensate :: SatInfo -> Either SatInfo Contradiction
+condensate info =
     let
+        sccS = map snd equivalences2
+        equivalences2 = equivalences info
         firstContradiction = dropWhile (not . opposite) sccS
-    in case firstContradiction of 
-        [] -> Left sccS
+        satGraph = [graph info]
+        newEdges =  concat $ buildEdge <$> satGraph  <*> equivalences2 <*> equivalences2
+        newGraph = buildG (1, maxLiteral info) newEdges
+    in case firstContradiction of
+        [] -> Left info{graph = newGraph}
         x:_  -> Right x
-
---condensateR:: Graph -> [Equivalence] -> (Graph, [(Int, Equivalence)])
-condensateR satGraph sccS = 
-    let
-        newComponents = zip ([1..] :: [Int]) sccS 
-        buildNewGraph = buildEdges <$> newComponents
-        --TODO: condensated graph check acyclic and skew-symetric
-        newGraph = buildG (1,2) [(1,2)]
-
-    in (newGraph, newComponents)
+{-|
+Look if two components are connected and build the corresponding edge.
+-}
+buildEdge :: Graph -> Equivalence -> Equivalence -> [Edge]
+buildEdge g (x,xs) (y,ys) = [(x,y) | x /= y, areConnected g xs ys]
 
 {-|
+Wether two components xs, ys have connected edges on the original formula graph.
 -}
-buildEdges :: (Int, [Int]) -> (Int, [Int]) -> Graph -> Maybe (Int, Int)
-buildEdges (x,xs) (y,ys) g = 
-    if areConnected g xs ys 
-        then Just (x,y) 
-        else Nothing 
-{-|
---wether tho components xs, ys have connected edges on the original formula
---TODO: maybe this is inefficient since there are repeated path executions
--}
-areConnected :: Graph -> [Int] -> [Int] -> Bool
+areConnected :: Graph -> Scc -> Scc -> Bool
 areConnected g xs ys = or (path g <$> xs <*> ys)
 
 {-|
@@ -103,20 +109,23 @@ opposite ls =  case ls of
     [x]    -> False
     []     -> False
 
-setValues x = case x of
-    Nothing -> Nothing
-    Just x -> Just (1,True)
+setValues g =
+    let
+        
+    in
+         [(1,True)]
 
 {-|
 Auxiliar function for reading formulas as lists.
 -}
-cnfToGraph :: Sat2 -> Graph
+cnfToGraph :: Sat2 -> ((Int,Int), Graph)
 cnfToGraph c =
     let
         vertList = concatMap (\[x,y]-> [(-x,y),(-y,x)]) c
         uppBound = maximum (map (uncurry max) vertList)
         lowBound = - uppBound
-    in buildG (lowBound, uppBound) vertList
+        bounds = (lowBound, uppBound)
+    in (bounds, buildG bounds vertList)
 
 {- drawVertexForest :: p -> Forest Vertex  -> String
 drawVertexForest x = 
@@ -128,10 +137,10 @@ drawVertexForest x =
 
 {-|
 Build a partial solution
-TODO
+
 Outputs the acyclic graph and the equivalences between variables so that multiple solutions can be found from them.
 -}
-findContradiction x y =
+{- findContradiction x y =
     let
         componentLs = map (sort . flatten) y
         contradiction = any opposite componentLs
@@ -144,6 +153,4 @@ findOpposite ls =  case ls of
     x:y:xs -> if x == negate y && x /= 0 then [x,y] else findOpposite (y:xs)
     [x]    -> []
     []     -> []
-
-ruta :: FilePath 
-ruta = "/home/alberto/github/2sat/src/Examples/cnf/"
+-}
